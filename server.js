118d502239132828391cda810aaffd3049315ebe
Ayan Abilbek <129643419+ayanvrsn@users.noreply.github.com>
2026-01-18 23:11:57 +0500

    featl: init Express server

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+const vehicleRoutes = require('./routes/vehicleRoutes');
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use(express.static('public')); 
+
+app.use('/api/vehicles', vehicleRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server: http://localhost:${PORT}`));
